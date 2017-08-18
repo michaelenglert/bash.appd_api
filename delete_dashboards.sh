cc3d2d6022eaf7f11942ed359f06832b373743f7
michael.englert <michi.eng@gmail.com>
2017-08-18 19:44:23 +0200

    delete dashboards

---
+++ delete_dashboards.sh
@@ -0,0 +1,20 @@
+source ./login.sh
+
+function apiCall {
+  $CURL_DEFAULT -b cookie.txt \
+                -X POST \
+                -H "X-CSRF-TOKEN: $XCSRFTOKEN" \
+                -H "Content-Type: application/json;charset=UTF-8" \
+                -H "Accept: application/json, text/plain, */*" \
+                -d "[$DASHBOARD_IDS]" \
+                $CONTROLLER_PREFIX$CONTROLLER_HOST:$CONTROLLER_PORT/controller/restui/dashboards/deleteDashboards
+}
+
+function readInput {
+  echo -n "Specify Dashboard IDs (e.g. 33,34): "
+  read DASHBOARD_IDS
+}
+
+readInput
+apiCall
+doneDone
