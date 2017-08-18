26224b2cfa31d5c5a1e9e78624825ecf271f6959
michael.englert <michi.eng@gmail.com>
2017-08-18 17:16:44 +0200

    export dashboard

---
+++ export_dashboards.sh
@@ -0,0 +1,19 @@
+source ./login.sh
+
+function apiCall {
+  $CURL_DEFAULT -b cookie.txt \
+                -X GET \
+                -H "X-CSRF-TOKEN: $XCSRFTOKEN" \
+                -H "Content-Type: application/json;charset=UTF-8" \
+                -H "Accept: application/json, text/plain, */*"\
+                $CONTROLLER_PREFIX$CONTROLLER_HOST:$CONTROLLER_PORT/controller/CustomDashboardImportExportServlet?dashboardId=$DASHBOARD_ID
+}
+
+function readInput {
+  echo -n "Specify Dashboard ID: "
+  read DASHBOARD_ID
+}
+
+readInput
+apiCall
+doneDone
