fede04a0729e5b4ac6b328932a7edd65ae0d802e
michael.englert <michi.eng@gmail.com>
2017-11-06 11:11:34 +0100

    added get apps

---
+++ get_apps.sh
@@ -0,0 +1,14 @@
+source ./login.sh
+
+function apiCall {
+  $CURL_DEFAULT -b cookie.txt \
+                -X GET \
+                -H "X-CSRF-TOKEN: $XCSRFTOKEN" \
+                -H "Cookie: JSESSIONID=$JSESSIONID; X-CSRF-TOKEN=$XCSRFTOKEN" \
+                -H "Content-Type: application/json" \
+                -H "Accept: application/json, text/plain, */*" \
+                $CONTROLLER_PREFIX$CONTROLLER_HOST:$CONTROLLER_PORT/restui/applicationManagerUiBean/getApplicationsAllTypes
+}
+
+apiCall
+doneDone
