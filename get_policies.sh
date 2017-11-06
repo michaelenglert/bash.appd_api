1203a1f5005c256dd2a1b2a034f36940366b7f07
michael.englert <michi.eng@gmail.com>
2017-11-06 11:12:38 +0100

    added get policies

---
+++ get_policies.sh
@@ -0,0 +1,20 @@
+source ./login.sh
+
+function apiCall {
+  $CURL_DEFAULT -b cookie.txt \
+                -X GET \
+                -H "X-CSRF-TOKEN: $XCSRFTOKEN" \
+                -H "Cookie: JSESSIONID=$JSESSIONID; X-CSRF-TOKEN=$XCSRFTOKEN" \
+                -H "Content-Type: application/json" \
+                -H "Accept: application/json, text/plain, */*" \
+                $CONTROLLER_PREFIX$CONTROLLER_HOST:$CONTROLLER_PORT/restui/policy/getPolicyListViewData/$APP_ID
+}
+
+function readInput {
+  echo -n "Specify App ID: "
+  read APP_ID
+}
+
+readInput
+apiCall
+doneDone
